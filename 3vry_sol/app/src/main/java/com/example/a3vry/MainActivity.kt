ef1f4c77340bcd84822f5c4f2c8bf6238197ee8a
Paweł Idzikowski <iddziku@gmail.com>
2020-06-09 20:05:06 +0200

    Update MainActivity.kt

--- 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a3vry
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -16,21 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-        val context = this
-
-        btn_insert.setOnClickListener {
-            if(name.text.toString().isNotEmpty()) {
-                var song = Song(name.text.toString())
-                var db = DbHandler(context)
-                db.insertSong(song)
-            } else {
-                Toast.makeText(context, "Please fill all data", Toast.LENGTH_SHORT).show()
-            }
+        viewBandsBtn.setOnClickListener {
+            val intent = Intent(this, BandsActivity::class.java)
+            startActivity(intent)
         }
     }
 
