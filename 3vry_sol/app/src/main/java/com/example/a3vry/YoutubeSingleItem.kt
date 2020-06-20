cfb1780f4db0ade9ab3c56dc2824d0256a4b2502
Paweł Idzikowski <iddziku@gmail.com>
2020-06-21 01:03:21 +0200

    Create YoutubeSingleItem.kt

---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/YoutubeSingleItem.kt
@@ -0,0 +1,14 @@
+package com.example.a3vry
+
+class YoutubeSingleItem {
+    val id : YtId? = null
+    val snippet : YtSnippet? = null
+}
+
+class YtId {
+    val videoId : String = ""
+}
+
+class YtSnippet {
+    val title : String = ""
+}
