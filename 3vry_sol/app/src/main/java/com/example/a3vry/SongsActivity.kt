62c7c222912014b394512a9e5dac39dc2be81a03
Paweł Idzikowski <iddziku@gmail.com>
2020-06-23 01:16:10 +0200

    Commit YoutubeApiService without KEY_API

--- 3vry_sol/app/src/main/java/com/example/a3vry/SongsActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/SongsActivity.kt
@@ -20,4 +20,18 @@ class SongsActivity : AppCompatActivity() {
         }
     }
 
+interface YouTubeApiService {
+    // Get results by query
+    @GET("search?key=$YOUTUBE_KEY_API&part=snippet&type=video&maxResults=50&topicId=/m/04rlf&videoCategoryId=10&order=viewCount&videoDuration=short")
+    fun results(@Query("q") q: String?): Call<YoutubeGetResponse>?
+    // Get results from app author playlist
+    @GET("playlistItems?key=$YOUTUBE_KEY_API&part=snippet&maxResults=50&playlistId=PLCrKXyV2OjXi2VF42Dimxvv9fnOH3JEl6")
+    fun playlistResults(): Call<YoutubeGetPlaylistResponse>?
+
+    // ********************************************************************
+    // BEFORE RUNNING 3VRY, MAKE SURE TO FILL IN YOUTUBE_KEY_API BELOW !! *
+    // ********************************************************************
+    companion object {
+        const val YOUTUBE_SEARCH_BASE_URL = "https://www.googleapis.com/youtube/v3/"
+    }
 }
