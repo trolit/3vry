a5fdf04be6ef09f357bef1a308313b68592913e1
Paweł Idzikowski <iddziku@gmail.com>
2020-06-22 23:16:21 +0200

    Add classes for playlist GET response

---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/YoutubeGetPlaylistResponse.kt
@@ -0,0 +1,5 @@
+package com.example.a3vry
+
+class YoutubeGetPlaylistResponse {
+    val items : List<YoutubeSinglePlaylistItem> = emptyList()
+}
---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/YoutubeSinglePlaylistItem.kt
@@ -0,0 +1,14 @@
+package com.example.a3vry
+
+class YoutubeSinglePlaylistItem {
+    val snippet : YtPlaylistSnippet? = null
+}
+
+class YtPlaylistSnippet {
+    val title : String = ""
+    val resourceId : YtPlaylistResourceId? = null
+}
+
+class YtPlaylistResourceId {
+    val videoId: String = ""
+}
