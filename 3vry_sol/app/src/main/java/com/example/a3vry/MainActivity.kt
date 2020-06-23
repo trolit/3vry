2635fa3af03ae12cb9726e40338e7d40f2a34f6a
Paweł Idzikowski <iddziku@gmail.com>
2020-06-23 20:01:34 +0200

    Add What and Why description

--- 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.a3vry
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +24,53 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SongsActivity::class.java)
             startActivity(intent)
         }
+
+        buttonWhat.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val text = "<font color='#16A085'>What is 3vry?</font><br/><br/><small>" +
+                    "Open source application created by <font color='#16A085'>github.com/trolit</font> using Kotlin " +
+                    "language that allows user to add favourite artists and get randomly single track offer each day from " +
+                    "YouTube. It's also possible to include app author playlist(visit Settings section).<br/><br/>3vry requires " +
+                    "Internet permission to make requests to YouTube service. App does not gather any personal data and source " +
+                    "code is available at:<br/><br/><font color='#16A085'>https://github.com/trolit/3vry</font></small>"
+
+            builder.setMessage(HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY))
+                .setPositiveButton(HtmlCompat.fromHtml("<font color='#16A085'>Ok</font>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                ) { _, _ ->
+                    // dismiss
+                }
+            builder.create()
+            builder.show()
+        }
+
+        buttonWhy.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val text = "<font color='#16A085'>Why 3vry?</font><br/><br/><small>" +
+                    "By making 3vry I mainly wanted to meet Kotlin and IntelliJ IDEA IDE after Xamarin.Forms project but " +
+                    "also make an app that will encourage to listen to at least one track each day. Listening to music has " +
+                    "powerful impact on human mind. Music is very crucial in games and movies. Maybe with this tool you will " +
+                    "find track(s) that you did not hear previously and get another favourite song(s)? Enjoy :)<br/><br/> " +
+                    "~Paweł Idzikowski(trolit), 23.06.2020</small>"
+            builder.setMessage(HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY))
+                .setPositiveButton(HtmlCompat.fromHtml("<font color='#16A085'>Ok</font>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                ) { _, _ ->
+                    // dismiss
+                }
+            builder.create()
+            builder.show()
+        }
+
+        val db = DbHandler(this)
+        val result = db.checkIfDbContainsAtLeastOneArtist()
+        if(result) {
+            db.checkForNewSong()
+        } else {
+            // override viewArtistsBtn functionality
+            viewSongsBtn.setOnClickListener {
+
+            }
+        }
+        db.close()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
