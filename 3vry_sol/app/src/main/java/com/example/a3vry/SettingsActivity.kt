c335773dfb05de2b46e42453d3f23db3fc6b4540
Paweł Idzikowski <iddziku@gmail.com>
2020-06-23 20:21:56 +0200

    Add Settings activity

---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a3vry
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
