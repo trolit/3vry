be8be1aa71c1c23d982d0b7831e3282d26f73cc8
Paweł Idzikowski <iddziku@gmail.com>
2020-06-26 01:12:07 +0200

    Add to Settings searching range changeable option

--- 3vry_sol/app/src/main/java/com/example/a3vry/SettingsActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/SettingsActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.text.HtmlCompat
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_settings.*
 
+
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,6 +62,56 @@ class SettingsActivity : AppCompatActivity() {
             swapButtons()
             setStatusOnTextView(this.getString(R.string.enabled), greenColor)
         }
+
+        when(db.getPrefValue(videoRange)) {
+            50 -> {
+                searchingRangeHeader.text =
+                    HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>50</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+            100 -> {
+                searchingRangeHeader.text =
+                    HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>100</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+            150 -> {
+                searchingRangeHeader.text =
+                    HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>150</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+            200 -> {
+                searchingRangeHeader.text =
+                    HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>200</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+        }
+
+        val options = arrayOf<CharSequence>("50", "100", "150", "200")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(this.getString(R.string.selectOption))
+        builder.setItems(options) { _, outcome ->
+            when (outcome) {
+                0 -> {
+                    db.updatePreference(Preference(videoRange, "50"))
+                    searchingRangeHeader.text =
+                        HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>50</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                }
+                1 -> {
+                    db.updatePreference(Preference(videoRange, "100"))
+                    searchingRangeHeader.text =
+                        HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>100</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                }
+                2 -> {
+                    db.updatePreference(Preference(videoRange, "150"))
+                    searchingRangeHeader.text =
+                        HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>150</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                }
+                3 -> {
+                    db.updatePreference(Preference(videoRange, "200"))
+                    searchingRangeHeader.text =
+                        HtmlCompat.fromHtml("${this.getString(R.string.rangeSettingHeader)} <strong>200</strong>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                }
+            }
+        }
+        changeSearchRangeBtn.setOnClickListener {
+            builder.show()
+        }
     }
 
     private fun setStatusOnTextView(status: String, color: String) {
@@ -70,5 +121,9 @@ class SettingsActivity : AppCompatActivity() {
     private fun swapButtons() {
         disablePlaylistBtn.isVisible = !disablePlaylistBtn.isVisible
         enablePlaylistBtn.isVisible = !enablePlaylistBtn.isVisible
+    }
+
+    companion object {
+        const val videoRange = "videoRange"
     }
 }
