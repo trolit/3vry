0704c42c8af8845b09617e503012be9a6c29246d
Paweł Idzikowski <iddziku@gmail.com>
2020-06-27 23:30:28 +0200

    Setup notification manager and use it if there is no Internet connection

--- 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.a3vry
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.text.HtmlCompat
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(returnNotificationChannel())
+
+        if(!checkNetworkState(this)) {
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_wifi)
+                .setContentTitle(HtmlCompat.fromHtml(this.getString(R.string.notificationTitle), HtmlCompat.FROM_HTML_MODE_LEGACY))
+                .setStyle(NotificationCompat.BigTextStyle().bigText(this.getString(R.string.notificationMsg)))
+                .setContentText(this.getString(R.string.notificationMsg))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
+        }
 
         viewArtistsBtn.setOnClickListener {
             startActivity(Intent(this, ArtistsActivity::class.java))
@@ -85,6 +107,16 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SongsActivity::class.java)
         startActivity(intent)
     }
+
+    // SOURCE: https://stackoverflow.com/questions/45711925/failed-to-post-notification-on-channel-null-target-api-is-26
+    private fun returnNotificationChannel() : NotificationChannel {
+        val mChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+        mChannel.enableLights(true)
+        mChannel.lightColor = Color.RED
+        mChannel.setShowBadge(false)
+        return mChannel
+    }
+
     // SOURCE: https://stackoverflow.com/questions/57284582/networkinfo-has-been-deprecated-by-api-29
     private fun checkNetworkState(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -100,5 +132,11 @@ class MainActivity : AppCompatActivity() {
             val nwInfo = connectivityManager.activeNetworkInfo ?: return false
             return nwInfo.isConnected
         }
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 0
+        const val CHANNEL_ID = "CH0"
+        const val CHANNEL_NAME = "CH"
     }
 }
