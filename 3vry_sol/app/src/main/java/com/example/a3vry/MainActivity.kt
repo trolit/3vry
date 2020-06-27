85e4d6d42db6dec630fe429234bebb380eb4c72e
Paweł Idzikowski <iddziku@gmail.com>
2020-06-27 23:29:30 +0200

    Add NetworkState verify method

--- 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
@@ -85,4 +85,20 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SongsActivity::class.java)
         startActivity(intent)
     }
+    // SOURCE: https://stackoverflow.com/questions/57284582/networkinfo-has-been-deprecated-by-api-29
+    private fun checkNetworkState(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
+    }
 }
