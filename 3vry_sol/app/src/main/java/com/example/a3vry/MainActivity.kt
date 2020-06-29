7efcee04f1956f8868c8a7c15d755a6d0e32b667
Paweł Idzikowski <iddziku@gmail.com>
2020-06-29 13:05:59 +0200

    Remove notification causing the app to crash and use TextView method to display status information

--- 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.a3vry
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
+import android.net.Network
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -22,19 +18,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Build notification and check for Internet availability
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(returnNotificationChannel())
-
-        if(!checkNetworkState(this)) {
-            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_wifi)
-                .setContentTitle(HtmlCompat.fromHtml(this.getString(R.string.notificationTitle), HtmlCompat.FROM_HTML_MODE_LEGACY))
-                .setStyle(NotificationCompat.BigTextStyle().bigText(this.getString(R.string.notificationMsg)))
-                .setContentText(this.getString(R.string.notificationMsg))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            notificationManager.notify(NOTIFICATION_ID, builder.build())
-        }
+        // SOURCE: https://stackoverflow.com/questions/25678216/android-internet-connectivity-change-listener
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager?.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                //take action when network connection is gained
+                runOnUiThread {
+                    noInternetConnTextView.isVisible = false
+                }
+            }
+            override fun onLost(network: Network?) {
+                //take action when network connection is lost
+                runOnUiThread {
+                    noInternetConnTextView.isVisible = true
+                }
+            }
+        })
 
         // Set UI buttons
         viewArtistsBtn.setOnClickListener {
@@ -71,7 +70,6 @@ class MainActivity : AppCompatActivity() {
         val db = DbHandler(this)
         val isArtistsTableNotEmpty = db.checkIfTableContainsAtLeastOneObject("Artists")
         val isSongsTableNotEmpty = db.checkIfTableContainsAtLeastOneObject("Songs")
-
         if(isArtistsTableNotEmpty) {
             db.checkForNewSong()
         } else if(!isArtistsTableNotEmpty && !isSongsTableNotEmpty) {
@@ -108,37 +106,5 @@ class MainActivity : AppCompatActivity() {
     private fun intentSongsActivity() {
         val intent = Intent(this, SongsActivity::class.java)
         startActivity(intent)
-    }
-
-    // SOURCE: https://stackoverflow.com/questions/45711925/failed-to-post-notification-on-channel-null-target-api-is-26
-    private fun returnNotificationChannel() : NotificationChannel {
-        val mChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
-        mChannel.enableLights(true)
-        mChannel.lightColor = Color.RED
-        mChannel.setShowBadge(false)
-        return mChannel
-    }
-
-    // SOURCE: https://stackoverflow.com/questions/57284582/networkinfo-has-been-deprecated-by-api-29
-    private fun checkNetworkState(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val nw      = connectivityManager.activeNetwork ?: return false
-            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-            return when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                else -> false
-            }
-        } else {
-            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-            return nwInfo.isConnected
-        }
-    }
-
-    companion object {
-        const val NOTIFICATION_ID = 0
-        const val CHANNEL_ID = "CH0"
-        const val CHANNEL_NAME = "CH"
     }
 }
