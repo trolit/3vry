fedda4581d28fb036202e95ec82205a55fc94f5c
Paweł Idzikowski <iddziku@gmail.com>
2020-07-20 18:13:48 +0200

    Create KeywordListAdapter.kt

---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/KeywordListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.a3vry
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.keywords_list_item.view.*
+
+class KeywordListAdapter(context: Context, var resource: Int, var listOfKeywords: MutableList<Keyword>) :
+    ArrayAdapter<Keyword>(context, resource, listOfKeywords) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater = LayoutInflater.from(context)
+        val view = convertView?:inflater.inflate(resource, null)
+
+        val textViewKeywordName = view.textViewKeywordName
+
+        val keyword = listOfKeywords[position]
+
+        textViewKeywordName.text = keyword.name
+
+        view.deleteKeywordBtn.setOnClickListener {
+            removeItem(context, position, keyword.id, keywords_TABLE_NAME, listOfKeywords)
+            notifyDataSetChanged()
+        }
+
+        return view
+    }
+}
