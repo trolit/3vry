b0347fa15689a67fcd246ec179e056060cd76cc9
Paweł Idzikowski <iddziku@gmail.com>
2020-07-20 19:06:48 +0200

    Change removeItem into buildAlertDialog

--- 3vry_sol/app/src/main/java/com/example/a3vry/HelperMethods.kt
+++ 3vry_sol/app/src/main/java/com/example/a3vry/HelperMethods.kt
@@ -4,17 +4,13 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 
-fun <E>removeItem(context: Context, pos: Int, rowId: Int, tableName: String, list: MutableList<E>) {
+fun buildAlertDialog(context: Context) : AlertDialog.Builder {
     val alertDialogBuilder = AlertDialog.Builder(context)
     alertDialogBuilder.setTitle(context.getString(R.string.confirmDeleteOperation))
-    alertDialogBuilder.setPositiveButton(context.getString(R.string.rawYes)) { _: DialogInterface, _: Int ->
-        val db = DbHandler(context)
-        db.deleteRowFromDb(rowId, tableName)
-        list.removeAt(pos)
-    }
+
     alertDialogBuilder.setNegativeButton(context.getString(R.string.rawNo)) { _: DialogInterface, _: Int ->
         // empty
     }
-    val alertDialog = alertDialogBuilder.create()
-    alertDialog.show()
+
+    return alertDialogBuilder
 }
