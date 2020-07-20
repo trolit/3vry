bf10f26d6992c5aac0341dbdcfc1d4f22386ba50
Paweł Idzikowski <iddziku@gmail.com>
2020-07-20 15:49:24 +0200

    Add class Keyword and Playlist

---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/Keyword.kt
@@ -0,0 +1,10 @@
+package com.example.a3vry
+
+class Keyword {
+    var id: Int = 0
+    var name: String = ""
+
+    constructor(name: String) {
+        this.name = name
+    }
+}
---
+++ 3vry_sol/app/src/main/java/com/example/a3vry/Playlist.kt
@@ -0,0 +1,10 @@
+package com.example.a3vry
+
+class Playlist {
+    var id: Int = 0
+    var playlistId: String = ""
+
+    constructor(playlistId: String) {
+        this.playlistId = playlistId
+    }
+}
